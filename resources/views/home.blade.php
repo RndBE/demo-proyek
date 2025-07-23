710ed9e753cb83433b8748ed08412f1eac9ae391
RndBE <rnd.software.be@gmail.com>
2025-07-23 15:31:47 +0700

    update layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Beranda')
+
+@section('content')
+    <h1>Selamat datang di Laravel!</h1>
+    <p>Ini halaman beranda dengan layout yang terstruktur.</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @stack('styles')
+
+    <meta charset="utf-8">
+    <link rel="icon" href="{{ asset('images/title.ico') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Midone admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="LEFT4CODE">
+    <title>{{ $title ?? 'Dashboard' }}</title>
+    <!-- BEGIN: CSS Assets-->
+    <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}" />
+    <!-- END: CSS Assets-->
+</head>
+
+    <body class="app">
+        <!-- BEGIN: Mobile Menu -->
+        @include('partials.mobile-menuquaility')
+        <!-- END: Mobile Menu -->
+        <div class="flex">
+            <!-- BEGIN: Side Menu -->
+            @include('partials.sidebar')
+
+            <!-- END: Side Menu -->
+            <!-- BEGIN: Content -->
+            <div class="content">
+                <!-- BEGIN: Top Bar -->
+                @include('partials.header')
+
+                @yield('content')
+            </div>
+        </div>
+        <!-- BEGIN: JS Assets-->
+        <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
+        <script src="{{ asset('dist/js/app.js') }}"></script>
+        <script src="https://unpkg.com/feather-icons"></script>
+        <!-- END: JS Assets-->
+    </body>
+</html>
